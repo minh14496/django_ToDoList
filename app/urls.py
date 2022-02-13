405e7847bbd97cf82edc69abf22b63f78d53c5fd
minh14496 <minh14496@gmail.com>
2022-02-13 14:23:23 -0800

    login/logout functionality

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,15 @@
 from ast import Del
 from django.urls import path
-from .views import TaskCreate, TaskList, TaskDetail, TaskUpdate, DeleteView
+from .views import TaskCreate, TaskList, TaskDetail, TaskUpdate, DeleteView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page="login"), name='logout'),
     path('', TaskList.as_view(), name='task'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete')
-
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,23 @@
-from dataclasses import field
+from dataclasses import field, fields
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
 from .models import Task
+
+
+class CustomLoginView(LoginView):
+    template_name = 'app/login.html'
+    fields = '__all___'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('task')
 
 
 class TaskList(ListView):
